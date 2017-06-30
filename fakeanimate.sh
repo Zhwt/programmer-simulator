68da42dc258ea1f8a1ea1de28437876fc4c20582
云淡风清 <greatzhwt@gmail.com>
2017-06-30 18:47:33 +0800

    now can print a fake file size

--- fakeanimate.sh
+++ fakeanimate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #usage: a fake animation that makes you looks like a programmer
-#can print package name flooding your terminal so far.
+#can print package name
+#and a fake file size
+#flooding your terminal so far.
 
 function show_package() {
     local line_count=$( wc -l < packagelist )
@@ -10,9 +12,33 @@ function show_package() {
     printf $p'\n'
 }
 
+function show_file_size() {
+    local r=$(( ( RANDOM % 500 ) + 1 ))
+    local s
+    local unit
+    if [[ $r -gt 100 ]];
+    then
+        unit="k"
+    elif [[ $r -gt 50 ]];
+    then
+        unit="k"
+    elif [[ $r -gt 10 ]];
+    then
+        unit="M"
+    else
+        s=$(( ( RANDOM % 9 ) ))
+        r=$r'.'$s
+        unit="M"
+    fi
+    printf $r' '$unit
+}
+
 while true
 do
     show_package
+    printf ' '
+    show_file_size
+    printf '\n'
     sleep 0.15
 done
 
